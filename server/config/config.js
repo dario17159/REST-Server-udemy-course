016d013b5cb23ac2bf6c402150d44253c6c0728d
Dario Carrizo <jose.2694.dc@gmail.com>
2019-10-13 18:02:34 -0300

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -9,7 +9,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://ponpon:bMtRYkg2GSj69jaM@cluster0-m7u7a.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URL
 }
 
 process.env.URLDB = urlDB;
